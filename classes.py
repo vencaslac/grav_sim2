4aad155da0fd608d413e4a9096aa9fbb73629227
vencaslac <vencaslac@gmail.com>
2018-09-28 23:18:23 +0300

    added docstrings to some functions

--- classes.py
+++ classes.py
@@ -10,16 +10,26 @@ from scipy.spatial.distance import cdist,euclidean
 
 
 @jit(parallel=True)
-def gforce(g,xa,xb,d,m1,m2):
+def gforce(g,x1,x2,d,m1,m2):
+    '''computes gforce between two particles on one axis using numba parellelization'''
 
-    return g*math.sin((xa-xb)/d)*(m1*m2/(d**2))
+    return g*math.sin((x1-x2)/d)*(m1*m2/(d**2))
 
 @jit(parallel=True)
 def g_from_part(g,m,d):
+    '''computes the field strength value of a particle of mass m at a distance d
+        given gravitational constant g
+    '''
     return g*m/d**2
 
 @jit(parallel=True)
 def build_field(g,masses,pixels,coords):
+    '''computes 2d gravitational field plot on a pixel array defined by the pixels
+        parameter given a list of masses, their coordinates and gravitational
+        constant g
+    '''
+    #TODO: FIX THIS FUNCTIOn
+
     p=np.zeros(pixels)
     for i in range(pixels[0]):
         for j in range(pixels[1]):
@@ -31,6 +41,10 @@ def build_field(g,masses,pixels,coords):
 
 @jit(parallel=True)
 def compute_gravity(G=0,masses=[],coords='',distances=''):
+    '''computes accelerations for each particle in the masses list via brute force
+        using numba to parellize nested loops
+    '''
+
     accelerations = []
     for i in range(len(masses)):
         fx = 0
@@ -74,6 +88,8 @@ class Sim:
                     }
 
     def apply_gravity(self):
+        '''applies gravity to every particle in the sim class'''
+
         masses=[]
         coords=[]
         acc=[]

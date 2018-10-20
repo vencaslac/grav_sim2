25def279cb325c012a59e8e053ab5600906db240
vencaslac <vencaslac@gmail.com>
2018-10-20 18:27:20 +0300

    added camera lock and focus toggle via m,n and space keys

--- entities/world.py
+++ entities/world.py
@@ -7,9 +7,12 @@ class World:
 
     def __init__(self):
         self.cfg=json.load(open('settings.cfg'))
+        self.win_size=[int(item) for item in self.cfg['Graphics']['window_size'].split(',')]
         self.sim=self.configure_sim()
         self.display=self.init_display()
         self.cpos=(0,0)
+        self.focus_particle=-1
+        self.camera_lock=False
 
     def configure_sim(self):
         ''' instantiates the simulation based on parameters given in
@@ -32,15 +35,18 @@ class World:
         if self.cfg['Graphics']['borderless'] == 'Yes':
             modes = modes | pygame.NOFRAME
 
-        win_size=self.cfg['Graphics']['window_size'].split(',')
-        win_size=[int(item) for item in win_size]
+        #self.win_size=self.cfg['Graphics']['window_size'].split(',')
+        #self.win_size=[int(item) for item in self.win_size]
 
-        return pygame.display.set_mode(win_size,modes)
+        return pygame.display.set_mode(self.win_size,modes)
 
     def update(self):
         '''updates the display'''
         self.sim.time_step()
         self.display.fill((0,0,0))
+        if self.camera_lock:
+            self.cpos = (int(round(self.sim.particles[self.focus_particle].coords[0]-self.win_size[0]//2)),\
+                         int(round(self.sim.particles[self.focus_particle].coords[1]-self.win_size[1]//2)))
         for p in self.sim.particles:
             try:
                 p.draw(self.display,self.cpos)
--- grav.py
+++ grav.py
@@ -5,6 +5,8 @@ import pygame
 
 w=world.World()
 egzit=False
+testpar=-1
+
 while not egzit:
     now=time.time()
     for event in pygame.event.get():
@@ -24,6 +26,26 @@ while not egzit:
             elif event.key == pygame.K_LEFT and pressed == False:
                 w.cpos = (w.cpos[0]-100,w.cpos[1])
                 pressed = True
+            elif event.key == pygame.K_m and pressed == False:
+                w.focus_particle +=1
+                if w.focus_particle > len(w.sim.particles)-1:
+                    w.focus_particle=0
+                pressed = True
+            elif event.key == pygame.K_n and pressed == False:
+                w.focus_particle -=1
+                if w.focus_particle < 0:
+                    w.focus_particle = len(w.sim.particles)-1
+                pressed = True
+            elif event.key == pygame.K_SPACE and pressed == False:
+                if w.camera_lock:
+                    w.camera_lock=False
+                else:
+                    w.camera_lock=True
+                pressed = True
+
+
+
+    print('{}|{}|{}'.format(str(w.cpos),str(w.sim.particles[testpar].coords),str(w.win_size)))
 
     w.update()
 
